509441c8cef4fb94589913111c3dcefc13f5b544
hpj <west_hangzhou@163.com>
2017-09-18 13:20:55 +0800

    user、role

--- demo-business/src/main/java/com/hup/dao/UserDAO.java
+++ demo-business/src/main/java/com/hup/dao/UserDAO.java
@@ -2,15 +2,24 @@ package com.hup.dao;
 
 
 import com.hup.entity.User;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 /**
- * Created by peppa pig
+ * <p>User: hup
+ * <p>Date: 14-1-28
+ * <p>Version: 1.0
  */
-public interface UserDAO {
+public interface UserDao {
 
-    List<User> getUserByUserName(@Param("userName") String userName);
+    public User createUser(User user);
+    public User updateUser(User user);
+    public void deleteUser(Long userId);
+
+    User findOne(Long userId);
+
+    List<User> findAll();
+
+    User findByUsername(String username);
 
 }
